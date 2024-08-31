1db065da8b2499ee13d6757bd6da9bef0080f119
Ramonprog <programador.ramon@gmail.com>
2024-08-31 16:45:58 -0300

    operadores relacionais

---
+++ operadores-relacionais/Program.cs
@@ -0,0 +1,20 @@
+﻿int number1 = 8;
+int number2 = 3;
+
+// maior
+Console.WriteLine(number1 > number2);
+
+// maior ou igual
+Console.WriteLine(number1 >= number2);
+
+// menor
+Console.WriteLine(number1 < number2);
+
+// menor ou igual
+Console.WriteLine(number1 <= number2);
+
+// igual
+Console.WriteLine(number1 == number2);
+
+// diferente
+Console.WriteLine(number1 != number2);
