ac4d2323edca16547a8b165acc1f13e2407ace02
Ramonprog <programador.ramon@gmail.com>
2024-08-31 16:54:31 -0300

    condicional simples

--- operadores-logicos/Program.cs
+++ operadores-logicos/Program.cs
@@ -2,7 +2,6 @@
 int number2 = 3;
 
 // E
-
 Console.WriteLine(number1 > number2 && number1 < number2);
 
 // OU
@@ -11,3 +10,17 @@ Console.WriteLine(number1 > number2 || number1 < number2);
 // NÃO
 Console.WriteLine(!true);
 
+
+// condicionais simples
+int idade = 29;
+
+if (idade > 18)
+{
+    Console.WriteLine("Você é maior de idade");
+}
+else 
+{
+    Console.WriteLine("Você é menor de idade");
+}
+
+// condicional encadeada
