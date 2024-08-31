0671484a5efd03d5a664df6c54a8d0a2e7347c3f
Ramonprog <programador.ramon@gmail.com>
2024-08-31 15:56:55 -0300

    readline + convertendo dados

--- constantes/Program.cs
+++ constantes/Program.cs
@@ -1,7 +1,10 @@
-﻿const string name = "alisson ramon";
+﻿// const
+const string name = "alisson ramon";
 const int age = 29;
 
 // concatenação 
 
-// Console.WriteLine("Meu nome é" + name + "e tenho" + age + "anos");
+Console.WriteLine("Meu nome é" + name + "e tenho " + age + " anos");
 Console.WriteLine($"Meu nome é {name} e tenho {age} anos");
+
+
---
+++ readline/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Olá qual o seu nome?");
+
+string ?name = Console.ReadLine();
+
+Console.WriteLine($"{name} quantos anos voçê tem?");
+
+int ?age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Olá " + name + ", você tem " + age + " anos");
+
+Console.WriteLine("Digite um número");
+int number = Convert.ToInt16(Console.ReadLine());
