cb8ed8a39d48d89b15766c5162ddfe3b864bce15
Ramonprog <programador.ramon@gmail.com>
2024-08-31 15:36:50 -0300

    variaveis

---
+++ variaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿// textos
+// é obrigatório colocar aspas duplas em textos maiores
+string name = "Alisson Ramon";
+
+// letras
+char letter = 'a';
+
+// numeros inteiros
+// não pode ter numero quebrados 2.9
+int age = 29;
+
+// numeros reais
+double salary = 29.99;
+
+// booleanos
+bool isStudent = true;
+
+Console.WriteLine(name);
+Console.WriteLine(letter);
+Console.WriteLine(age);
+Console.WriteLine(salary);
+Console.WriteLine(isStudent);
