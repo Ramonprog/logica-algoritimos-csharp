33422ee5a02b83a809ab3634bbfa7ff07b753738
Ramonprog <programador.ramon@gmail.com>
2024-08-31 16:24:47 -0300

    operadores aritimeticos

---
+++ operadores-aritimeticos/Program.cs
@@ -0,0 +1,10 @@
+﻿int number1 = 30;
+int number2 = 3;
+
+Console.WriteLine(number1 + number2);
+Console.WriteLine(number1 - number2);
+Console.WriteLine(number1 * number2);
+Console.WriteLine(number1 / number2);
+Console.WriteLine(number1 % number2);
+Console.WriteLine(number1++);
+Console.WriteLine(number1--);
