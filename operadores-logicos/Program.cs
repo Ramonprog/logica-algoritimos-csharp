34ae589a4e5b8110e96a7119b7d22db782bc14f7
Ramonprog <programador.ramon@gmail.com>
2024-08-31 16:48:51 -0300

    operadores logicos

---
+++ operadores-logicos/Program.cs
@@ -0,0 +1,13 @@
+﻿int number1 = 8;
+int number2 = 3;
+
+// E
+
+Console.WriteLine(number1 > number2 && number1 < number2);
+
+// OU
+Console.WriteLine(number1 > number2 || number1 < number2);
+
+// NÃO
+Console.WriteLine(!true);
+
