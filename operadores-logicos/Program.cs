421928c43e0214ebcbba5471ea16f2b96b8060e6
Ramonprog <programador.ramon@gmail.com>
2024-08-31 16:57:47 -0300

    condicionais encadeadas

--- operadores-logicos/Program.cs
+++ operadores-logicos/Program.cs
@@ -24,3 +24,18 @@ else
 }
 
 // condicional encadeada
+
+double media = 6;
+
+if (media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else if (media >=5)
+{
+    Console.WriteLine("Em exame");
+}
+else 
+{
+    Console.WriteLine("Reprovado");
+}
